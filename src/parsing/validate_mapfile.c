c296627801d7672af4e01e605d4f9d61e9e5506e
Claudia <sofiagil2288@gmail.com>
2025-10-15 13:54:09 +0200

    fix error .cub

--- src/parsing/validate_mapfile.c
+++ src/parsing/validate_mapfile.c
@@ -6,7 +6,7 @@
 /*   By: claudia <claudia@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 13:17:54 by claudia           #+#    #+#             */
-/*   Updated: 2025/10/14 12:31:12 by claudia          ###   ########.fr       */
+/*   Updated: 2025/10/15 13:51:07 by claudia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	validate_file(char *filename)
 	int	len;
 
 	len = ft_strlen(filename);
-	if (len < 4)
+	if (len <= 4)
 		return (-1);
 	 if (ft_strncmp(filename + len - 4, ".cub", 4) == 0
         || ft_strncmp(filename + len - 4, ".xpm", 4) == 0)
@@ -47,7 +47,7 @@ int validate_map(char *map)
 		return (-1);
 	if (check_open(map) == -1)
 	{
-		printf("error: could not open file\n");
+		printf("Error: could not open file\n");
 		return (-1);
 	}
 	return (0);
