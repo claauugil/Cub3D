a5fc25d75a42fdb5643df3cef310d5e331428ce1
Claudia <sofiagil2288@gmail.com>
2025-10-16 12:32:35 +0200

    some parsing

--- src/parsing/map/handle_map.c
+++ src/parsing/map/handle_map.c
@@ -6,23 +6,36 @@
 /*   By: claudia <claudia@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/15 10:14:40 by claudia           #+#    #+#             */
-/*   Updated: 2025/10/15 15:19:10 by claudia          ###   ########.fr       */
+/*   Updated: 2025/10/15 18:17:07 by claudia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-static int validate_map_chars(char **map_lines);
-static int validate_single_player(char **lines);
-
-int handle_map(char **lines, t_congif *cfg, int map_start)
+static int count_map_size(char **lines, t_congif *cfg)
 {
-    (void)cfg;
-    if (validate_map_chars(lines + map_start) == -1)
-        return (ft_print_error("Error: invalid chars"));
-    if (validate_single_player(lines + map_start) == -1)
-        return (ft_print_error("More than a single player detected"));
-    return (0);
+	int i;
+	int len;
+
+	if (!lines || !cfg)
+		return (-1);
+	i = 0;
+	cfg->map_width = 0;
+	cfg->map_height = 0;
+
+	while(lines[i])
+	{
+		len = 0;
+		while (lines[i][len])
+			len++;
+		if (len > cfg->map_width)
+			cfg->map_width = len;
+		cfg->map_height++;
+		i++;
+	}
+	if (cfg->map_height == 0 || cfg->map_width == 0)
+		return (-1);
+	return (0);
 }
 
 static int validate_single_player(char **lines)
@@ -72,4 +85,57 @@ static int validate_map_chars(char **map_lines)
 	}
 	printf("Valid map chars!\n");
 	return (0);
+}
+
+static int allocate_map(t_congif *cfg)
+{
+	int i;
+	int	j;
+
+	i = 0;
+	if (!cfg || cfg->map_height <= 0 || cfg->map_width <= 0)
+		return (-1);
+	cfg->map = malloc(sizeof(char *) * cfg->map_height);
+	if (!cfg->map)
+		return (-1);
+	while (i < cfg->map_height)
+	{
+		cfg->map[i] = malloc(sizeof(char *) * (cfg->map_width + 1));
+		if (!cfg->map[i])
+		{
+			while (i-- > 0)
+				free(cfg->map[i]);
+			free(cfg->map);
+			cfg->map = NULL;
+			return (-1);
+		}
+		j = 0;
+		while (j < cfg->map_width)
+		{
+			cfg->map[i][j] = ' ';
+			j++;
+		}
+		cfg->map[i][cfg->map_width] = '\0';
+		i++;
+	}
+	return (0);
+}
+
+int handle_map(char **lines, t_congif *cfg, int map_start)
+{
+    (void)cfg;
+    if (validate_map_chars(lines + map_start) == -1)
+		return (ft_print_error("Error: invalid chars"));
+    if (validate_single_player(lines + map_start) == -1)
+		return (ft_print_error("More than a single player detected"));
+	if (count_map_size(lines + map_start, cfg) == -1)
+		return (-1);
+	if (allocate_map(cfg) == -1)
+		return (-1);
+	/*if (copy_map(lines + map_start) == -1)
+		return (-1);
+	if (check_map(cfg) == -1)
+		return (-1);
+	normalize_map(cfg);*/
+    return (0);
 }
